5aba82c966a793b1b0348c5b949a3671a58b312a
myung6024 <myung6024@gmail.com>
2018-12-14 00:23:46 +0900

    splash화면, 앱 이미지 변경

---
+++ app/src/main/java/com/project/myung/boostcampproject/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.project.myung.boostcampproject
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+        finish()
+    }
+}
